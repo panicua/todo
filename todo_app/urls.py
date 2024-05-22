1d52f8def19df5fde2568d4eae311ed96e076ab3
kekW <panicua12@gmail.com>
2024-05-22 11:38:42 +0300

    add view and corresponding url for tag list page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from todo_app.views import index
+from todo_app.views import index, TagListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo"
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.views import generic
+
+from todo_app.models import Tag
 
 
 def index(request):
     context = {}
     return render(request, "todo/index.html", context=context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+    template_name = "todo/tag_list.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TagListView, self).get_context_data(**kwargs)
+        # name = self.request.GET.get("name", "")
+        # context["search_form"] = TagSearchForm(initial={"name": name})
+        return context
+
+    # def get_queryset(self):
+    #     form = TagSearchForm(self.request.GET)
+    #     queryset = Tag.objects.all()
+    #     if form.is_valid():
+    #         return queryset.filter(name__icontains=form.cleaned_data["name"])
+    #     return queryset
