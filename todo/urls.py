b8dbeb7b07c18906587e03087855fed9e8d46fe4
kekW <panicua12@gmail.com>
2024-05-22 14:15:51 +0300

    add default urls with django.contib.auth.urls

--- todo/urls.py
+++ todo/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from todo import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("todo_app.urls", namespace="todo")),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+    path("accounts/", include("django.contrib.auth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
