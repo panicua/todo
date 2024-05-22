4b150300a402025ebbc2a6c868564f98c8d329e6
kekW <panicua12@gmail.com>
2024-05-22 12:24:49 +0300

    add widget datetime for dead_line_time field, removed is_done field

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,9 +1,26 @@
 from django import forms
+from django.forms import DateTimeInput
 
-from todo_app.models import Tag
+from todo_app.models import Tag, Task
 
 
 class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
         fields = ["name"]
+
+
+class TaskForm(forms.ModelForm):
+    dead_line_time = forms.DateTimeField(
+        widget=DateTimeInput(
+            format="%Y-%m-%d %H:%M:%S",
+            attrs={
+                "type": "datetime-local"
+            }
+        )
+    )
+
+    class Meta:
+        model = Task
+        fields = ["content", "dead_line_time", "tags"]
+
